b705b6a217d9701190124a3f4c5caa63cf146b01
Alexander Ibekason <alibekason@gmail.com>
2022-03-11 09:39:08 +0100

    add js introduction

---
+++ introduction/index.js
@@ -0,0 +1,7 @@
+document.getElementById("para").style.backgroundColor = "red";
+
+document.getElementById("para").innerHTML = "<b style='color: white'>Changed InnerHtml </b>";
+
+document.write("<b>I added this javascript code</b>");
+
+console.log("Learning javascript")
