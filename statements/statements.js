4c6a516f032a992696881eb221eb8ea66c1bf7c9
Alexander Ibekason <alibekason@gmail.com>
2022-03-11 10:11:39 +0100

    add js statements

---
+++ statements/statements.js
@@ -0,0 +1,9 @@
+let pie = 22/7;
+let radius = 30;
+let circumference = 2 * pie * radius;
+
+console.log(circumference);
+
+let paragraph = document.getElementById("para").innerHTML;
+
+console.log(paragraph);
