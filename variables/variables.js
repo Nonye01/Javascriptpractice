d28281ded3eba0310f88b71f27067e74fc6aae51
Ibekason Alexander <alibekason@gmail.com>
2022-03-24 22:42:14 +0100

    Add varibales

---
+++ variables/variables.js
@@ -0,0 +1,11 @@
+let myName = "Alex";
+var yourName = "Chinonye";
+const mathPie = 3.142;
+
+myName = "Alexander"
+yourName = "Obioha"
+// mathPie = 3
+
+console.log(myName);
+console.log(yourName);
+console.log(mathPie);
